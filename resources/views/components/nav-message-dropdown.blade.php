2310dc202675b7ee49ee397bd83a02898e7484f8
gvelizlasteniasoftware <gustavo.veliz@lasteniasoftware.com.ar>
2023-11-08 16:03:00 -0300

    update seccion mensajes

--- resources/views/components/nav-message-dropdown.blade.php
+++ resources/views/components/nav-message-dropdown.blade.php
@@ -9,14 +9,14 @@
         <iconify-icon class="text-slate-800 dark:text-white text-xl" icon="heroicons-outline:mail"></iconify-icon>
         <span class="absolute -right-1 lg:top-0 -top-[6px] h-4 w-4 bg-red-500 text-[8px] font-semibold flex flex-col items-center
             justify-center rounded-full text-white z-[45]">
-        10</span>
+        1</span>
     </button>
     <!-- Mail Dropdown -->
     <div class="dropdown-menu z-10 hidden bg-white divide-y divide-slate-100 dark:divide-slate-900 shadow w-[335px]
         dark:bg-slate-800 border dark:border-slate-900 !top-[18px] rounded-md overflow-hidden lrt:origin-top-right rtl:origin-top-left">
         <div class="flex items-center justify-between py-4 px-4">
-        <h3 class="text-sm font-Inter font-medium text-slate-700 dark:text-white">Messages</h3>
-        <a class="text-xs font-Inter font-normal underline text-slate-500 dark:text-white" href="#">See All</a>
+        <h3 class="text-sm font-Inter font-medium text-slate-700 dark:text-white"><span style="color: red">Mensajes de Alerta</span></h3>
+        <a class="text-xs font-Inter font-normal underline text-slate-500 dark:text-white" href="#">Ver todos</a>
         </div>
         <div class="divide-y divide-slate-100 dark:divide-slate-900" role="none">
         <div class="text-slate-600 dark:text-slate-300 block w-full px-4 py-2 text-sm">
@@ -26,7 +26,7 @@
                 <span class="bg-secondary-500 w-[10px] h-[10px] rounded-full border border-white dark:border-slate-700 inline-block absolute
                     right-0 top-0"></span>
                 <img
-                    src="/images/all-img/user.png"
+                    src="/images/icon/fire-station_yellow.png"
                     alt="user"
                     class="block w-full h-full object-cover rounded-full border hover:border-white border-transparent">
                 </div>
@@ -36,10 +36,15 @@
                 href="#"
                 class="text-slate-800 dark:text-slate-300 text-sm font-medium mb-1 before:w-full before:h-full before:absolute
                     before:top-0 before:left-0">
-                Wade Warren</a>
-                <div class="text-xs hover:text-[#68768A] text-slate-600 dark:text-slate-300 mb-1">Hi! How are you doing?.....</div>
+                ZONA 1</a>
+                <div class="text-xs hover:text-[#68768A] text-slate-600 dark:text-slate-300 mb-1 font-bold">Patiño, Formosa</div>
+                <div class="text-xs hover:text-[#68768A] text-slate-800 dark:text-slate-800 mb-1">Detecci&oacute;n temprana de posible foco</div>
+                <div class="text-xs hover:text-[#68768A] text-slate-800 dark:text-slate-800 mb-1">Coordenadas: <span> -25.04494, -60.55855</span></div>
+                <div class="text-xs hover:text-[#68768A] text-slate-600 dark:text-slate-300 mb-1"><span style="color: black">EMERGENCIAS:</span></div>
+                <div class="text-xs hover:text-[#68768A] text-slate-600 dark:text-slate-300 mb-1">Telefono: <a href="tel:0370 442-8888">0370 442-8888</a></div>
+                <div class="text-xs hover:text-[#68768A] text-slate-600 dark:text-slate-300 mb-1">Direcci&oacute;n: P3600 Formosa</div>
                 <div class="text-slate-400 dark:text-slate-400 text-xs">
-                3 min ago</div>
+                15 min ago</div>
             </div>
             <div class="flex-0">
                 <span class="h-4 w-4 bg-danger-500 border border-white rounded-full text-[10px] flex items-center justify-center text-white">
@@ -47,7 +52,7 @@
             </div>
             </div>
         </div>
-        <div class="text-slate-600 dark:text-slate-300 block w-full px-4 py-2 text-sm  cursor-pointer">
+        {{--<div class="text-slate-600 dark:text-slate-300 block w-full px-4 py-2 text-sm  cursor-pointer">
             <div class="flex ltr:text-left rtl:text-right space-x-3 rtl:space-x-reverse relative">
             <div class="flex-none">
                 <div class="h-8 w-8 bg-white dark:bg-slate-700 rounded-full relative">
@@ -70,8 +75,8 @@
                 </div>
             </div>
             </div>
-        </div>
-        <div class="text-slate-600 dark:text-slate-300 block w-full px-4 py-2 text-sm  cursor-pointer">
+        </div>--}}
+        {{--<div class="text-slate-600 dark:text-slate-300 block w-full px-4 py-2 text-sm  cursor-pointer">
             <div class="flex ltr:text-left rtl:text-right space-x-3 rtl:space-x-reverse relative">
             <div class="flex-none">
                 <div class="h-8 w-8 bg-white dark:bg-slate-700 rounded-full relative">
@@ -99,7 +104,7 @@
                 8</span>
             </div>
             </div>
-        </div>
+        </div>--}}
         </div>
     </div>
 </div>
