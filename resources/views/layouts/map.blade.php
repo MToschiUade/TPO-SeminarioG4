423bdfc0c0aadd1dbf2975eeef4d3d60522c48b7
gvelizlasteniasoftware <gustavo.veliz@lasteniasoftware.com.ar>
2023-11-19 18:49:08 -0300

    update modal

--- resources/views/layouts/map.blade.php
+++ resources/views/layouts/map.blade.php
@@ -54,29 +54,38 @@
                 <div class="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
                     <div class="relative bg-white rounded-lg shadow dark:bg-slate-700">
                         <!-- Modal header -->
-                        <div class="flex items-center justify-between p-5 border-b rounded-t dark:border-slate-600 bg-black-500">
+                        <div class="flex items-center justify-between p-5 border-b rounded-t dark:border-slate-600" style="background-color: yellow">
                             <h3 class="text-xl font-medium dark:text-white capitalize">
-                                Basic Modal
+                                Alerta Amarilla
                             </h3>
-                            <button type="button" class="text-slate-400 bg-transparent hover:text-slate-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-slate-600 dark:hover:text-white" data-bs-dismiss="modal">
-                                <svg aria-hidden="true" class="w-5 h-5" fill="#ffffff" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-                                </svg>
-                                <span class="sr-only">Close modal</span>
-                            </button>
                         </div>
                         <!-- Modal body -->
                         <div class="p-6 space-y-4">
                             <h6 class="text-base text-slate-900 dark:text-white leading-6">
-                                Lorem ipsum dolor sit.
+                                ZONA 1: Campo La Libertad
                             </h6>
                             <p class="text-base text-slate-600 dark:text-slate-400 leading-6">
-                                Oat cake ice cream candy chocolate cake apple pie. Brownie carrot cake candy canes. Cake sweet roll cake cheesecake cookie chocolate cake liquorice.
+                                Deteccion temprana de posible foco <br>
+                                Coordenadas: -25.04494, -60.55855<br>
+                            </p>
+                            <h6 class="text-base text-slate-900 dark:text-white leading-6">
+                                CLIMA
+                            </h6>
+                            <p class="text-base text-slate-600 dark:text-slate-400 leading-6">
+                                Deteccion temprana de posible foco <br>
+                                Coordenadas: <br>
+                            </p>
+                            <h6 class="text-base text-slate-900 dark:text-white leading-6">
+                                EMERGENCIAS
+                            </h6>
+                            <p>
+                                Telefono: <a href="tel:0370 442-8888">0370 442-8888</a><br>
+                                Direcci&oacute;n: P3600 Formosa
                             </p>
                         </div>
                         <!-- Modal footer -->
                         <div class="flex items-center justify-end p-6 space-x-2 border-t border-slate-200 rounded-b dark:border-slate-600">
-                            <button data-bs-dismiss="modal" class="btn inline-flex justify-center bg-black-500" onclick="closeModal()">Accept</button>
+                            <button data-bs-dismiss="modal" class="btn inline-flex text-white justify-center bg-slate-900" onclick="closeModal()">Cerrar</button>
                         </div>
                     </div>
                 </div>
