bb245a622de1dd156b049c7e2e517b41e059951a
gvelizlasteniasoftware <gustavo.veliz@lasteniasoftware.com.ar>
2023-11-20 13:42:33 -0300

    update cambio de imagen

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -9,24 +9,26 @@
         {{-- Scripts --}}
         @vite(['resources/css/app.scss', 'resources/js/custom/store.js'])
     </head>
-    <body>
+    <body  style="background-image: url('{{@asset('images/fondo.jpeg')}}')">
 
         <div class="loginwrapper">
             <div class="lg-inner-column">
-                <div class="left-column relative z-[1]">
+                <div class="left-column relative z-[1]" style="background-image: url('{{@asset('images/fondo.jpeg')}}')">
                     <div class="max-w-[520px] pt-20 ltr:pl-20 rtl:pr-20">
                         <!-- APPLICATION LOGO -->
                         <div class="mb-6">
                             <x-application-logo />
                         </div>
-                        <h4>
-                            <span class="text-slate-800 dark:text-slate-400 font-bold">SAT.IA</span>
-                            <br/> Sistema de Alerta Temprana de Incendios en Argentina
-                        </h4>
+                        <h1 style="text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;">
+                            <span class="text-slate-800 dark:text-slate-400 font-bold text-white">SAT.IA</span>
+                        </h1>
+                        <h3 style="text-shadow: -0.7px 0 white, 0 0.7px white, 0.7px 0 white, 0 -0.7px white;">
+                            Sistema de Alerta Temprana de Incendios en Argentina
+                        </h3>
                     </div>
                     <div class="absolute left-0 2xl:bottom-[-160px] bottom-[-130px] h-full w-full z-[-1]">
 {{--                        <img class="h-full w-full object-containll" src="{{ getSettings('guest_background') }}" alt="image">--}}
-                        <img class="h-full w-full object-containll" src="{{ @asset('images/svg/Icono-Fortigate.svg') }}" alt="image">
+{{--                        <img class="h-full w-full object-containll" src="{{ @asset('images/svg/Icono-Fortigate.svg') }}" alt="image">--}}
                     </div>
                 </div>
                 <div class="right-column  relative">
